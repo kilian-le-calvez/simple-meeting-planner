623454d197743c30ddb31b89255c3d29e43876b3
Kilian Le Calvez <kilianlecalvez.dev@gmail.com>
2025-11-07 11:57:22 +0100

    downgrade tw v3 + remove rolldown experimental

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
-import { defineConfig } from "tailwindcss";
-
-export default defineConfig({
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-});
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
